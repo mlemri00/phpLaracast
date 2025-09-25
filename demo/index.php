458328290373ebbfacdff1356e49b56516bb4491
lemr <lemrissanimohammed@gmail.com>
2025-09-25 22:36:41 +0200

    If else i maneres diferents de imprimir variables

--- demo/index.php
+++ demo/index.php
@@ -3,14 +3,29 @@
 <head>
     <meta charset="UTF-8">
     <title>Title</title>
+    <style>
+        body{
+            display:grid;
+            place-items:center;
+            height: 100vh;
+            margin:0;
+            font-family: sans-serif;
+        }
+    </style>
 </head>
 <body>
-    <h1>
     <?php
-    $saludo="hola";
+        $nom = "Memorias del subsuelo";
+        $llegit = true;
 
-    echo $saludo."World!";
+        if ($llegit) {
+            $missatge = "Tú m'has llegit";
+        }else{
+            $missatge = "Tú no m'has llegit";
+        }
     ?>
+    <h1><?php echo $missatge; ?>
+        <?=$missatge?>
     </h1>
 
 </body>
