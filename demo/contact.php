e73b456886b9ab280a021b03ef26a6b42eacee77
lemr <mlemrissani1056@alumnes.politecnicllevant.cat>
2025-10-10 17:30:15 +0200

    Fer que una parcial sigui dinamica per cada página de vista

--- demo/contact.php
+++ demo/contact.php
@@ -1,3 +1,6 @@
 <?php
 
+$heading="Contact";
+
+
 require "views/contact.view.php";
--- demo/index.php
+++ demo/index.php
@@ -1,13 +1,5 @@
 <?php
 
+$heading="Home";
 
-$negoci = [
-  "nom"=>"Laracast",
-  "preu"=>"15",
-  "categories"=>["Testing","PHP","Javascript"]
-];
-
-function registrar($usuari){
-
-};
 require "views/index.view.php";
