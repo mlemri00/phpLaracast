29614950f1e86cb1410ca8ea437844ab1eade8d9
lemr <mlemrissani1056@alumnes.politecnicllevant.cat>
2025-10-10 20:10:56 +0200

    Refactorització de passar a clau valor el enrutador i fer una resposta 404

--- demo/index.php
+++ demo/index.php
@@ -2,12 +2,26 @@
 
 require ("functions.php");
 
-$uri = $_SERVER['REQUEST_URI'];
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+/*//el _SERVER es variable global que te una llista de variables,
+ el REQUEST... es per demanar al servidor la URI, utilitzam la funcio
+parse_url per pasar un string i demanar nomes la ruta, per així que el
+enrutador sempre tengui la informació correcta
 
-if ($uri === '/'){
-    require 'controllers/index.php';
-}else if ($uri === '/about'){
-    require 'controllers/about.php';
-}else if ($uri === '/contact'){
-    require 'controllers/contact.php';
+*/
+
+$routes = [
+  '/'=>'controllers/index.php',
+  '/about'=>'controllers/about.php',
+  '/contact'=>'controllers/contact.php'
+];
+
+if(array_key_exists($uri,$routes)){
+    require $routes[$uri];
+}else{
+    http_response_code(404);
+
+    require 'views/404.php';
+
+    die();
 }
---
+++ demo/views/404.php
@@ -0,0 +1,12 @@
+<?= require('partials/head.php')?>
+<?= require ("partials/nav.php")?>
+<main>
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <!-- Your content -->
+        <h1 class="text-2xl font-bold">Sorry. Page Not Found.</h1>
+        <p class="mt-4">
+            <a href="/" class="text-blue underline">Go back home.</a>
+        </p>
+    </div>
+</main>
+<?= require ('partials/footer.php')?>
