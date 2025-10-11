f10cb15478b7785d3ae75281f49e7c0033c4c97e
lemr <mlemrissani1056@alumnes.politecnicllevant.cat>
2025-10-11 07:33:35 +0200

    Pasar tot el objecte PDO a la classe Database.php

--- demo/index.php
+++ demo/index.php
@@ -2,30 +2,9 @@
 
 // require ("router.php");
 require ("functions.php");
+require ("Database.php");
 
 
-class Database{
-
-
-    public $connection;
-
-    public function __construct(){
-        $dsn = 'mysql:host=127.0.0.1;port=3309;user=root;password=123456789;dbname=myapp;charset=utf8mb4';
-        // PHP data object
-        $this->connection = new PDO($dsn);
-
-    }
-
-    public function query($query){
-        $statement = $this->connection->prepare($query);
-        $statement->execute();
-
-        return $statement;
-
-    }
-
-
-}
 
 $db=new Database();
 
