d4e5ba7eeb2023cd333eb1a25144c8cbdc11418e
lemr <mlemrissani1056@alumnes.politecnicllevant.cat>
2025-10-11 07:14:44 +0200

    Fer la primera request a una base de dades mysql

--- demo/index.php
+++ demo/index.php
@@ -1,11 +1,19 @@
 <?php
 
-require ("router.php");
+// require ("router.php");
 require ("functions.php");
 
-class Person{
-    public $name;
-    public $age;
+//Preparar connexió
+$dsn = 'mysql:host=127.0.0.1;port=3309;user=root;password=123456789;dbname=myapp;charset=utf8mb4';
+// PHP data object
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare("select * from posts");
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
 
 
+foreach ($posts as $post){
+    echo "<li>". $post['title']."</li>";
 }
