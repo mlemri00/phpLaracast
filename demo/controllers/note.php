7e5c5feea0034f2fe77669f2d5737725b9f04d44
lemr <mlemrissani1056@alumnes.politecnicllevant.cat>
2025-10-28 09:35:01 +0100

    Restringir acces a les notes de altres usuaris

--- demo/controllers/note.php
+++ demo/controllers/note.php
@@ -6,5 +6,16 @@ $dba = new Database($config);
 
 $heading = "Note";
 
-$note = $dba->query('select * from notes where id = :id',['id'=>$_GET['id']])->fetch() ;
+$note = $dba->query('select * from notes where id = :id',[
+    'id'=>$_GET['id']
+])->fetch() ;
+
+if (! $note){
+    abort();
+}
+if ($note['user_id']!=1){
+    abort(403);
+}
+
+
 require "views/note.view.php";
