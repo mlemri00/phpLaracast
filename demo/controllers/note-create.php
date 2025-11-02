d393b87bd131d125ab00e84e76535c5d8a94bfa4
lemr <mlemrissani1056@alumnes.politecnicllevant.cat>
2025-11-02 10:01:32 +0100

    Crear el form per crear notes

--- demo/controllers/note-create.php
+++ demo/controllers/note-create.php
@@ -1,3 +1,11 @@
 <?php
 $heading='Create note';
+
+if ($_SERVER['REQUEST_METHOD']=='POST'){
+
+}
+
+
+
+
 require 'views/note-create.view.php';
--- demo/views/note-create.view.php
+++ demo/views/note-create.view.php
@@ -4,7 +4,24 @@
 
     <main>
         <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <h1>Create a note</h1>
+            <form method="post">
+                <div class="space-y-12">
+                    <div class="border-b border-white/10 pb-12">
+                        <div class="col-span-full">
+                                <label for="body" class="block text-sm/6 font-medium text-white">Body</label>
+                                <div class="mt-2">
+                                    <textarea id="body" name="about" rows="3" class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"></textarea>
+                                </div>
+                                <p class="mt-3 text-sm/6 text-gray-400">Write a few sentences about yourself.</p>
+                            </div>
+                        </div>
+                    </div>
+                <div class="mt-6 flex items-center justify-end gap-x-6">
+                    <button type="button" class="text-sm/6 font-semibold text-white">Cancel</button>
+                    <button type="submit" class="rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Save</button>
+                </div>
+            </form>
+
         </div>
     </main>
 <?= require ('partials/footer.php')?>
