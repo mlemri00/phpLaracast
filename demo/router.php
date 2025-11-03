1392c92857430994b4e1baa73a0a7d6359e21174
lemr <mlemrissani1056@alumnes.politecnicllevant.cat>
2025-11-03 11:06:12 +0100

    Arreglar el path dels archius de vista

--- demo/router.php
+++ demo/router.php
@@ -22,7 +22,7 @@ function routeToController($uri,$routes){
 
 function abort($code = 404 ){// això és per fer un paràmetre de sèrie si no es passa ni un
     http_response_code($code);
-    require "views/{$code}.php";
+    require base_path("views/{$code}.php");
 
     die();
 }
--- demo/views/404.php
+++ demo/views/404.php
@@ -1,5 +1,5 @@
-<?= require(base_path('partials/head.php'))?>
-<?= require (base_path("partials/nav.php"))?>
+<?= require(base_path('views/partials/head.php'))?>
+<?= require (base_path("views/partials/nav.php"))?>
 <main>
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
         <!-- Your content -->
