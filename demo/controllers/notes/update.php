8396d09d3bc8e521322a96145f5e434e36215376
lemr <mlemrissani1056@alumnes.politecnicllevant.cat>
2025-11-03 18:13:38 +0100

    Fer el patch de les notes amb la base de dades en l'arxiu de update.php

--- demo/controllers/notes/update.php
+++ demo/controllers/notes/update.php
@@ -1,2 +1,42 @@
 <?php
-;
+
+use core\Database;
+use core\App;
+use core\Validator;
+
+
+$db = App::resolve(Database::class);
+
+
+$currentUserId = 2;
+
+$note = $db -> query('select * from notes where id = :id',[
+    'id'=>$_POST['id']
+])->findOrFail();
+
+
+authorize($note['user_id']===$currentUserId);
+
+$errors = [];
+
+if (!Validator::string($_POST['body'],1,1000)){
+    $errors['body']='A body of no more than 1000 characters,  is required';
+}
+
+
+if (count($errors)){
+    return view('notes/edit.view.php',[
+        'heading'=>'Edit Note',
+        'errors'=>$errors,
+        'note'=>$note
+    ]);
+}
+
+$db -> query('update notes set body = :body where id = :id',[
+
+    'body' => $_POST['body']
+    , 'id'=>$_POST['id']
+]);
+
+header('location: /notes');
+die();
--- demo/views/notes/edit.view.php
+++ demo/views/notes/edit.view.php
@@ -5,7 +5,12 @@
     <main>
         <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
             <form method="POST" action="/note">
+
+
                 <input type="hidden" name="_method" value="PATCH">
+                <input type="hidden" name="id" value="<?=$note['id']?>">
+
+
                 <div class="space-y-12">
                     <div class="border-b border-white/10 pb-12">
                         <div class="col-span-full">
