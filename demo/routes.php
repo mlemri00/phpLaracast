7c440164e4ab0f0eded325175c8c927a08ce4cf7
lemr <mlemrissani1056@alumnes.politecnicllevant.cat>
2025-11-04 09:45:54 +0100

    Prova de l'enrutament a register i store.php

--- demo/routes.php
+++ demo/routes.php
@@ -4,13 +4,19 @@
 
 $router->get('/','controllers/index.php');
 $router->get( '/about','controllers/about.php');
+
 $router->get('/notes','controllers/notes/index.php',);
 $router->get('/note','controllers/notes/show.php');
+
 $router->get('/notes/create','controllers/notes/create.php');
 $router->delete('/note','controllers/notes/destroy.php');
+
 $router->get('/note/edit','controllers/notes/edit.php');
 $router->patch('/note','controllers/notes/update.php');
 
 
 $router->get('/contact','controllers/contact.php');
 $router->post('/note','controllers/notes/store.php');
+
+$router->get('/register','controllers/registration/create.php');
+$router->post('/register','controllers/registration/store.php');
