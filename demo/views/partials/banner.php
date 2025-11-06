f744aa89570e22ed95a3a4c34211a366096c0cf8
lemr <mlemrissani1056@alumnes.politecnicllevant.cat>
2025-11-06 17:51:51 +0100

    Guardar usuaris en la base de dades

--- demo/views/partials/banner.php
+++ demo/views/partials/banner.php
@@ -2,7 +2,6 @@
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
         <h1 class="text-3xl font-bold tracking-tight text-white">
 
-        Hello, <?= $_SESSION['name']?>
         </h1>
     </div>
 </header>
