08cf8b47b33567ca145b99aee1121d0383b7c668
lemr <mlemrissani1056@alumnes.politecnicllevant.cat>
2025-11-08 09:51:04 +0100

    Fer el hash per les constransenyes

--- demo/views/index.view.php
+++ demo/views/index.view.php
@@ -4,7 +4,7 @@
 
     <main>
         <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <!-- Your content -->
+            <p class="text-white" >Hello, <?= $_SESSION['user']['email']?? 'guest' ?></p>
         </div>
     </main>
 
