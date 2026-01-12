5bef5251a14e5d4b7479383360856326d6895697
lemr <mlemrissani1056@alumnes.politecnicllevant.cat>
2026-01-12 13:57:16 +0100

    Creació del mètodes has, put , get

--- demo/core/Session.php
+++ demo/core/Session.php
@@ -5,4 +5,21 @@ namespace core;
 class Session
 {
 
+    public static function has($key){
+        return (bool) self::get($key);
+    }
+
+    public static function put($key, $value){
+        $_SESSION[$key] = $value;
+    }
+
+    public static function get($key, $default = null){
+        return $_SESSION['_flash'][$key] ?? $_SESSION[$key] ?? $default;
+    }
+
+
+
+
+
+
 }
