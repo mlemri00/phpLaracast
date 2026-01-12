6616041d5ff65082c20ffb7d6d876a4505b9f996
lemr <mlemrissani1056@alumnes.politecnicllevant.cat>
2026-01-12 14:11:48 +0100

    Creació dels mètodes flash, unflash, flush y destroy a la classe Session

--- demo/core/Session.php
+++ demo/core/Session.php
@@ -17,7 +17,24 @@ class Session
         return $_SESSION['_flash'][$key] ?? $_SESSION[$key] ?? $default;
     }
 
+    public static function flash($key, $value){
+        $_SESSION['_flash'][$key] = $value;
+    }
 
+    public static function unflash(){
+        unset($_SESSION['_flash']);
+    }
+
+    public static function flush(){
+        $_SESSION = [];
+    }
+
+    public static function destroy (){
+        self::flush();
+        session_destroy();
+        $params = session_get_cookie_params();
+        setcookie('PHPSESSID','',time()-3600, $params['domain'],$params['secure'],$params['httponly']);
+    }
 
 
 
